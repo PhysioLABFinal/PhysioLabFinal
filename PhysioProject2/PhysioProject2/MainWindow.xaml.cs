6ad9e02801cfaae2436cf3d1b6b14bdbfd7d2fe7
Dimitris Gagoutis <dimitros96@gmail.com>
2017-12-14 22:04:21 +0200

    new

---
+++ PhysioProject2/PhysioProject2/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhysioProject2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void client_button(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Clients();
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void products_button(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Products();
+        }
+
+        private void appointm_button(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Appointments();
+        }
+
+        private void staff_button(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Staff();
+        }
+
+        private void payments_button(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Payments();
+        }
+    }
+}
