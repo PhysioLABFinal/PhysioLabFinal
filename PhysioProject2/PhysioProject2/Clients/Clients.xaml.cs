640f309f1e7c23fdfac9ad413835f154b6aa2f1a
mariannaparadi <marianna_paradi@hotmail.com>
2017-12-25 00:41:56 +0200

    connect methods with database

--- PhysioProject2/PhysioProject2/Clients/Clients.xaml.cs
+++ PhysioProject2/PhysioProject2/Clients/Clients.xaml.cs
@@ -1,26 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PhysioProject2
 {
-    /// <summary>
-    /// Interaction logic for Clients.xaml
-    /// </summary>
-    public partial class Clients : Page
+	/// <summary>
+	/// Interaction logic for Clients.xaml
+	/// </summary>
+	public partial class Clients : Page
     {
 
 		OleDbConnection con;
@@ -62,10 +53,80 @@ namespace PhysioProject2
             info.Show();
         }
 
-        private void addclieck(object sender, RoutedEventArgs e)
-        {
-            Window1 win = new Window1();
-            win.Show();
-        }
-    }
+
+		private void SearchTxt_KeyDown(object sender, KeyEventArgs e)
+		{
+			DataView dv = dt.DefaultView;
+			dv.RowFilter = string.Format("CSurname like '*{0}*'", SearchTxt.Text);
+			ClientdataGrid.ItemsSource = dv;
+		}
+
+		private void addClient_Click(object sender, RoutedEventArgs e)
+		{
+			OleDbCommand cmd = new OleDbCommand();
+			if (con.State != ConnectionState.Open)
+				con.Open();
+			cmd.Connection = con;
+
+			if (clientIDTxt.Text != "")
+			{
+				if (clientIDTxt.IsEnabled == true)
+				{
+					cmd.CommandText = "insert into ClientTableDB(CID,Cname,CSurname,Birthday,Telephone,Doctor,Diagnosis) Values(" + clientIDTxt.Text + ",'" + clientNameTxt.Text + "','" + clientSurnameTxt.Text + "','" + clientBirthdayTxt.Text + "','" + clientPhoneTxt.Text + "','" + clientDoctorTxt.Text + "','" + clientDiagnosisTxt.Text + "')";
+					cmd.ExecuteNonQuery();
+					BindGrid();
+					MessageBox.Show("Ο νέος πελάτης προστέθηκε με επιτυχία!!");
+					ClearAll();
+				}
+				else
+				{
+					cmd.CommandText = "update ClientTableDB set Cname='" + clientNameTxt.Text + "',CSurname='" + clientSurnameTxt.Text + "',Birthday='" + clientBirthdayTxt.Text + "',Telephone='" + clientPhoneTxt.Text + "',Doctor='" + clientDoctorTxt.Text + "',Diagnosis='" + clientDiagnosisTxt.Text + "' where CID=" + clientIDTxt.Text;
+					cmd.ExecuteNonQuery();
+					BindGrid();
+					MessageBox.Show("Οι αλλαγές έγιναν με επιτυχία!!");
+					ClearAll();
+				}
+			}
+			else
+			{
+				MessageBox.Show("Παρακαλώ προσθέστε ID πελάτη.......");
+			}
+		}
+
+		private void ClearAll()
+		{
+			clientIDTxt.Text = "";
+			clientNameTxt.Text = "";
+			clientSurnameTxt.Text = "";
+			clientBirthdayTxt.Text = "";
+			clientPhoneTxt.Text = "";
+			clientDoctorTxt.Text = "";
+			clientDiagnosisTxt.Text = "";
+			addClient.Content = "Add";
+			clientIDTxt.IsEnabled = true;
+		}
+
+		private void editClient_Click(object sender, RoutedEventArgs e)
+		{
+			if (ClientdataGrid.SelectedItems.Count > 0)
+			{
+				DataRowView row = (DataRowView)ClientdataGrid.SelectedItems[0];
+				clientIDTxt.Text = row["CID"].ToString();
+				clientNameTxt.Text = row["Cname"].ToString();
+				clientSurnameTxt.Text = row["CSurname"].ToString();
+				clientBirthdayTxt.Text = row["Birthday"].ToString();
+				clientPhoneTxt.Text = row["Telephone"].ToString();
+				clientDoctorTxt.Text = row["Doctor"].ToString();
+				clientDiagnosisTxt.Text = row["Diagnosis"].ToString();
+				clientIDTxt.IsEnabled = false;
+				addClient.Content = "Update";
+			}
+			else
+			{
+				MessageBox.Show("Παρακαλώ επιλέξτε ένα πελάτη από τη λίστα...");
+			}
+		}
+
+
+	}
 }
