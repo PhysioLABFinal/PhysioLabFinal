da0b66f093919a17edcc82d4e594930e85761817
Dimitris Gagoutis <dimitros96@gmail.com>
2017-12-30 01:03:59 +0200

    Products

--- PhysioProject2/PhysioProject2/Products/Products.xaml.cs
+++ PhysioProject2/PhysioProject2/Products/Products.xaml.cs
@@ -12,20 +12,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
-namespace PhysioLab.Products
+
+namespace PhysioProject2
 {
     /// <summary>
     /// Interaction logic for Products.xaml
     /// </summary>
     public partial class Products : Page
     {
+        OleDbConnection con;
+        DataTable dt;
+
         public Products()
         {
             InitializeComponent();
+            
+            this.DataContext = this;
+
+
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=.\\PhysioDatabase.accdb"; //" + AppDomain.CurrentDomain.BaseDirectory + "
+            BindGrid();
+
         }
 
+        private void BindGrid()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Products";
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            ProductsDataGrid.ItemsSource = dt.AsDataView();
+        }
 
-
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
     }
 }
