6373da5fd1ddc16e482a65ecaff4b980558c9a4b
Dimitris Gagoutis <dimitros96@gmail.com>
2018-01-09 06:06:23 +0200

    Late Night Fixes

--- PhysioProject2/PhysioProject2/Documentation.xaml.cs
+++ PhysioProject2/PhysioProject2/Documentation.xaml.cs
@@ -25,7 +25,7 @@ namespace PhysioProject2
         public Documentation()
         {
             InitializeComponent();
-            string pathh = "..\\..\\README_PhysioLAb.docx";
+            string pathh = "README_PhysioLAb.docx";
             Process wordProcess = new Process();
             wordProcess.StartInfo.FileName =pathh;
             wordProcess.StartInfo.UseShellExecute = true;
